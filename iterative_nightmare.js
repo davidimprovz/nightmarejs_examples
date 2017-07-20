9156d4cd9c745cf57855217d1d970420806635a7
David Williams <david@comfedesigns.com>
2017-07-20 11:31:35 -0400

    adding some tests and clarifying variable names

--- iterative_nightmare.js
+++ iterative_nightmare.js
@@ -12,74 +12,74 @@ const url = 'http://www.nasdaq.com/markets/stocks/symbol-change-history.aspx';
 
 // create js class to hold the data and links for inspection
 function tickerChanges(selector, clicker){
-	// set the browsing selectors
-	this.selector = selector;
-	this.clicker = clicker;
+  // set the browsing selectors
+  this.selector = selector;
+  this.clicker = clicker;
 
-	// data bins
-	this.data = []; 
-	this.links = [];
-	
-	// setter methods
-	var addData = (data) => this.data.append(data);
-	var addLink = (link) => this.links.append(link);
+  // data bins
+  this.data = []; 
+  this.links = [];
+  
+  // setter methods
+  var addData = (data) => this.data.append(data);
+  var addLink = (link) => this.links.append(link);
 
-	// getter methods
-	var getData = () => this.data;
-	var getLinks = () => this.links;
-	
-	// save data methods
-	var saveData = () => {
-		fs.writeFile('scraped_stockchange_data.js', JSON.stringify(this.data));
-		// empty this.data to free memory
-		return 'saved data!';
-	}
-	var saveLinks = () => {
-		fs.writeFile('scraped_stockchange_links.js', JSON.stringify(this.links));
-		// empty this.links to free memory
-		return 'saved links!';
-	}
+  // getter methods
+  var getData = () => this.data;
+  var getLinks = () => this.links;
+  
+  // save data methods
+  var saveData = () => {
+    fs.writeFile('scraped_stockchange_data.js', JSON.stringify(this.data));
+    // empty this.data to free memory
+    return 'saved data!';
+  }
+  var saveLinks = () => {
+    fs.writeFile('scraped_stockchange_links.js', JSON.stringify(this.links));
+    // empty this.links to free memory
+    return 'saved links!';
+  }
 }
 
 function iterateNightmare(url){
-	console.log('scrape started');
+  console.log('scrape started');
   let nightmare = Nightmare({ show: false }); // get a nightmare instance
 
   nightmare.goto(url)
-  	.wait('table.SymbolChangeList')
-  	.evaluate(function(data_dock){
-  		console.log('scrape started');
-  		console.log(data_dock.selector);
-  		// check if there's a link to follow and add it to the stack
-  		let obj = {
-		    tableData: Array.from(document.querySelectorAll(data_dock.selector))
-          					.map((element) => element.innerText),
-		    nextPage: document.getElementById(data_dock.clicker)[0].href ? document.getElementsByClassName('next')[0].href : 'none',
-	    }
-	    console.log('fist scrape');
-    	return obj;
+    .wait('table.SymbolChangeList')
+    .evaluate(function(changes){
+      console.log('scrape started');
+      console.log(changes.selector);
+      // check if there's a link to follow and add it to the stack
+      let obj = {
+        tableData: Array.from(document.querySelectorAll(changes.selector))
+                    .map((element) => element.innerText),
+        nextPage: document.getElementById(changes.clicker)[0].href ? document.getElementsByClassName('next')[0].href : 'none',
+      }
+      console.log('fist scrape');
+      return obj;
     }, changes)
     .end()
-    .run(function(error, result, data_dock) { // get the results...data_dock is the changes class instance
+    .run(function(error, result, changes) { // get the results...data_dock is the changes class instance
       if (error) { console.error(error); } // look for errors
       else { // if no errs
-      	console.log('formatting started');
-        data_dock.addData(result.tableData); // use setter to append scraped data
+        console.log('formatting started');
+        changes.addData(result.tableData); // use setter to append scraped data
 
-        if (result.nextPage != 'none') { // if there are more pages	
-        	data_dock.addLink(result.nextPage); // use setter to append scraped link
-        	console.log( `Now scraping ${result.nextPage}` );
-        	iterateNightmare(result.nextPage)
+        if (result.nextPage != 'none') { // if there are more pages 
+          changes.addLink(result.nextPage); // use setter to append scraped link
+          console.log( `Now scraping ${result.nextPage}` );
+          iterateNightmare(result.nextPage)
         }
         else {
-        	console.log( `Scraped ${arr.length + 1} pages` ); // count out # of page scrapes
-        	console.log( data_dock.saveData() ); // save the data to disk and report
-        	console.log( data_dock.saveLinks() ); // save the links to disk and report
+          console.log( `Scraped ${arr.length + 1} pages` ); // count out # of page scrapes
+          console.log( changes.saveData() ); // save the data to disk and report
+          console.log( changes.saveLinks() ); // save the links to disk and report
         }
       }
     }, changes)
     .catch(function(err){
-    	if (err){ console.log(err); }
+      if (err){ console.log(err); }
     });
 }
 
@@ -87,6 +87,7 @@ function iterateNightmare(url){
 const select = 'table.SymbolChangeList td'; // a collection of child elems
 const clicker = 'two_column_main_content_lb_NextPage'; // an element id
 var changes = new tickerChanges(select, clicker);
+console.log(changes.selector);
 
 console.log('starting the scrape');
 iterateNightmare(url);
